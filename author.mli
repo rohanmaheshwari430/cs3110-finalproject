e9ca0f35aa854465e0ca8bb96c163f74eff5d757
Rohan Maheshwari <rm697@cornell.edu>
2021-03-22 19:08:45 -0400

    initialized ocaml project

---
+++ author.mli
@@ -0,0 +1,36 @@
+(** CS3110 Spring 2021 Final Project - Command Line Tool for CMS
+
+@author Rohan Maheshwari rm697
+@author Sukriti Sudhakar ss2742
+@author Samiksha Hiranandani snh44
+
+*)
+
+(************************************************************ 
+
+   Academic Integrity Statement
+
+   We, the people named in the @author comment above, have fully reviewed the
+   course academic integrity policies. We have adhered to those policies in
+   solving the assignment.
+
+   The policies do permit some limited collaboration among students currently
+   enrolled in the course. If we did engage in such collaboration, here is the
+   list of other students with whom we collaborated, and a brief summary of that
+   collaboration:
+
+   - none
+
+   Here are any deviations from the policies that we want to document, with the
+   understanding that being honest about my mistakes is more honorable than
+   lying about our intellectual work:
+
+   - none
+
+ ************************************************************)
+
+
+(** [hours_worked] is the number of hours we worked on this project. *)
+val hours_worked : int
+
+
